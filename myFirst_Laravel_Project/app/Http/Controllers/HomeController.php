fe048650d3112eea2d85c861dc09b41218f14540
Milka Bakhuya <bakhuyamilka@gmail.com>
2023-05-20 07:46:37 +0300

    Created message model

--- myFirst_Laravel_Project/app/Http/Controllers/HomeController.php
+++ myFirst_Laravel_Project/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $message=DB::table('messages')->where('user_id',1)->get();
+        dd($message);
         return view('home');
     }
 }
--- myFirst_Laravel_Project/resources/views/home.blade.php
+++ myFirst_Laravel_Project/resources/views/home.blade.php
@@ -5,18 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Recent Updates') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                   <form action="" method="post"></form>
+                   <textarea name="body" class="form-control" rows="3" placeholder="what's on your mind"></textarea>
+                   <button class="btn btn-primary">post</button>
                 </div>
+                <hr>
+               
             </div>
+            post
         </div>
     </div>
 </div>
